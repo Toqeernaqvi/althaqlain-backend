c014cdee124a66c5419b57fac159b17b428d5082
ToqeerNaqvi <toqeernaqvi66@gmail.com>
2024-07-06 02:18:03 +0500

    update hotel

--- app/controllers/api/hotels_controller.rb
+++ app/controllers/api/hotels_controller.rb
@@ -1,2 +1,22 @@
+# app/controllers/api/hotels_controller.rb
 class Api::HotelsController < ApplicationController
- end  
+    before_action :set_hotel, only: [:show]
+  
+    # GET /api/hotels
+    def index
+      @hotels = Hotel.all
+      render json: @hotels
+    end
+  
+    # GET /api/hotels/:id
+    def show
+      render json: @hotel
+    end
+  
+    private
+  
+    def set_hotel
+      @hotel = Hotel.find(params[:id])
+    end
+  end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
     delete 'delete_image/:image_id', action: :delete_image, as: 'delete_image', on: :member
 
   end
+  namespace :api do
+    resources :hotels, only: [:index, :show]
+  end
 
   root 'hotels#index'
 end
