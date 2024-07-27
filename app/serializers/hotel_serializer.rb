0595a86d9b596744bf67d8867fc9752b1d4c80e6
ToqeerNaqvi <toqeernaqvi66@gmail.com>
2024-07-27 13:15:33 +0500

    update code

--- app/serializers/hotel_serializer.rb
+++ app/serializers/hotel_serializer.rb
@@ -1,15 +1,17 @@
 # app/serializers/hotel_serializer.rb
 class HotelSerializer < ActiveModel::Serializer
-    include Rails.application.routes.url_helpers
-  
-    attributes :id, :name, :city, :country, :state, :address, :loc_lat, :loc_long, :rating, :price, :images_urls, :base_image_url
-  
-    def images_urls
-      object.images.map { |image| rails_blob_url(image, only_path: true) } if object.images.attached?
-    end
-  
-    def base_image_url
-      rails_blob_url(object.base_image, only_path: true) if object.base_image.attached?
-    end
+  include Rails.application.routes.url_helpers
+
+  attributes :id, :name, :city, :country, :state, :address, :loc_lat, :loc_long, :rating, 
+             :price, :discounted_price, :bed, :living_room, :bathroom, :kitchen, 
+             :reserved_room, :facilities, :paragraphs, :created_at, :updated_at, 
+             :base_image_url, :images_urls
+
+  def images_urls
+    object.images.map { |image| rails_blob_url(image, only_path: true) } if object.images.attached?
   end
-  
+
+  def base_image_url
+    rails_blob_url(object.base_image, only_path: true) if object.base_image.attached?
+  end
+end
