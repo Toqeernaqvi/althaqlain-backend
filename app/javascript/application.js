453d04b8bfd24acb7a873a7288b6e53911f91016
Toqeer Abbas <toqeer@techsembly.com>
2024-08-13 21:59:24 +0500

    fix

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,3 @@
-//= require jquery_ujs
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
